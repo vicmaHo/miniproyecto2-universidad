a12815ef3fd6652a97ca2a106e8a4f546708d137
Victor Hernandez <victormanu968@gmail.com>
2023-05-16 10:12:20 -0500

    Modificacion a CategoriaDulce e implementacion de Lista

--- src/modelo/CategoriaDulce.java
+++ src/modelo/CategoriaDulce.java
@@ -1,7 +1,17 @@
 package modelo;
 
 public enum CategoriaDulce {
-    DULCE,
-    ACIDO,
-    SINAZUCAR
+    DULCE("Dulce"),
+    ACIDO("Acido"),
+    SINAZUCAR("Sin Azucar");
+
+    private final String nombre;
+
+    CategoriaDulce(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getElementoFormateado() {
+        return nombre;
+    }
 }
